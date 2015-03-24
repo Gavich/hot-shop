84eb25191dfc882ce605b983d2276879a693ac42
Gavich <gavaab@gmail.com>
2015-03-25 00:17:08 +0200

    add #64 issaleble
    add #64 issaleble

--- app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
+++ app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
@@ -413,18 +413,31 @@ class Mage_Catalog_Model_Product_Type_Configurable extends Mage_Catalog_Model_Pr
     public function isSalable($product = null)
     {
         $salable = parent::isSalable($product);
+
         if ($salable !== false) {
             $salable = false;
             if (!is_null($product)) {
                 $this->setStoreFilter($product->getStoreId(), $product);
             }
-            foreach ($this->getUsedProducts(null, $product) as $child) {
-                if ($child->isSalable()) {
+
+            if (!Mage::app()->getStore()->isAdmin() && $product) {
+                $collection = $this->getUsedProductCollection($product)
+                    ->addAttributeToFilter('status', Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+                    ->setPageSize(1)
+                ;
+                if ($collection->getFirstItem()->getId()) {
                     $salable = true;
-                    break;
+                }
+            } else {
+                foreach ($this->getUsedProducts(null, $product) as $child) {
+                    if ($child->isSalable()) {
+                        $salable = true;
+                        break;
+                    }
                 }
             }
         }
+
         return $salable;
     }
     /**
